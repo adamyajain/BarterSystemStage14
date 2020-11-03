0a3863b89fe2b558f691ad9ac18590e5b4ad89fb
adamyajain <apoorvadoc@gmail.com>
2020-11-03 22:02:54 +0530

    config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCBpqEkWbN_UlEqN47AOwrUOooQ_Z9du4k",
+    authDomain: "bartersystem-990c0.firebaseapp.com",
+    databaseURL: "https://bartersystem-990c0.firebaseio.com",
+    projectId: "bartersystem-990c0",
+    storageBucket: "bartersystem-990c0.appspot.com",
+    messagingSenderId: "550007464504",
+    appId: "1:550007464504:web:0520c5ac8a3d788cd2bccd",
+    measurementId: "G-6WZL8QBP54"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
